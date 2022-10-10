91d205eacedc4b8258d476a6901cbec5768c49d1
dongchan <windcj1@naver.com>
2022-10-10 23:41:23 +0900

    1010 TIL

--- algorithm study/1006_baekjoon_15591.py
+++ algorithm study/1006_baekjoon_15591.py
@@ -1,53 +1,39 @@
 # MooTube
-
-def dijk(s, k):
-    visited = [0]*(N+1)
-    d = [INF]*(N+1)
-    d[s] = 0
-
-    for _ in range(N-1):
-        u = 0
-        minV = INF
-        for i in range(1, N+1):
-            if d[i] < minV and not visited[i]:
-                u = i
-                minV = d[i]
-
-        visited[u] = 1
-
-        for j in range(1, N+1):
-            if adjM[u][j] != INF and not visited[j]:
-                if d[u] != 0:                           # u가 시작 정점이 아니면
-                    d[j] = min(d[j], d[u], adjM[u][j])  #
-                else:
-                    d[j] = min(d[j], adjM[u][j])
-                if d[j] < k:
-                    break
+def bfs(k, s):
+    USADO = [INF]*(N+1)
+    USADO[s] = 0
 
     cnt = 0
-    for i in d[1:]:
-        if i >= k and i != INF:
-            cnt += 1
+    for i in range(1, N+1):
+        if i != s:                        # 자기 자신을 제외한 모든 정점 i에 대해서
+            if adjM[s][i] != -1:      # s와 i가 인접했다면
+                USADO[i] = adjM[s][i]
+            else:                         # s와 i가 인접하지 않았다면
+                 for j in range(1, N+1):
+                    if adjM[i][j] != -1 and adjM[s][j] != -1:   # 어떤 정점 j가 s와도 인접하고 i와도 인접한
+                        USADO[i] = min(USADO[j], adjM[i][j])
 
+            if USADO[i] >= k:
+                cnt += 1
+    # print(USADO)
     return cnt
 
 from sys import stdin
+from collections import deque
 N, Q = map(int, stdin.readline().split())
+INF = 1000000000 + 1
+adjM = [[-1]*(N+1) for _ in range(N+1)]
 
-INF = 1000000000+1
-adjM = [[INF]*(N+1) for _ in range(N+1)]
 for _ in range(N-1):
     p, q, r = map(int, stdin.readline().split())
-    if adjM[p][q] > r:
+    if adjM[p][q] == -1 or adjM[p][q] > r:
         adjM[p][q] = r
-    if adjM[q][p] > r:
+    if adjM[q][p] == -1 or adjM[q][p] > r:
         adjM[q][p] = r
 
+# for i in range(N+1):
+#     adjM[i][i] = 0
 
-quests = []
 for _ in range(Q):
     k, v = map(int, stdin.readline().split())
-    print(dijk(v, k))
-
-# v를 시작 정점으로 하는 그래프에서 최소가중치가 k이상인 것은 몇 개인가?
-
+    print(bfs(k, v))
--- algorithm study/prac.py
+++ algorithm study/prac.py
@@ -1,3 +1,60 @@
-N, M = 6, 4
-visited = [[[0]*2 for _ in '_'*M] for _ in '_'*N]
-print(visited)
+# MooTube
+def bfs(k, s):
+    visited = [0]*(N+1)
+    visited[s] = 1
+    USADO = [INF] * (N + 1)
+    USADO[s] = 0
+    check = 0
+    not_connected = list(range(1, N+1))
+    for v, w in adjL[s]:
+        USADO[v] = min(USADO[v], w)
+        visited[v] = 1
+        check += 1
+        not_connected.remove(v)
+    not_connected.remove(s)
+
+    while check != N-1:
+        for i in not_connected:
+            for v, w in adjL[i]:
+                if visited[v]:
+                    USADO[i] = min(USADO[v], w)
+                    visited[i] = 1
+                    not_connected.remove(i)
+                    check += 1
+
+    cnt = 0
+    for i in USADO[1:]:
+        if i >= k:
+            cnt += 1
+    # print(USADO)
+    return cnt
+
+
+from sys import stdin
+from collections import deque
+
+N, Q = map(int, stdin.readline().split())
+INF = 1000000000 + 1
+
+adjL = [[] for _ in range(N + 1)]
+for _ in range(N - 1):
+    p, q, r = map(int, stdin.readline().split())
+    adjL[p].append((q, r))
+    adjL[q].append((p, r))
+# [[], [(2, 3)], [(1, 3), (3, 2), (4, 4)], [(2, 2)], [(2, 4)]]
+# print(adjL)
+for _ in range(Q):
+    k, v = map(int, stdin.readline().split())
+    print(bfs(k, v))
+
+'''
+5 3
+1 2 3
+2 3 2
+2 4 4
+5 4 1
+1 2
+4 1
+2 5
+
+'''
