d4ec542a615e48526bbd9aba3f0741c191655edd
dongchan <windcj1@naver.com>
2022-10-18 23:55:28 +0900

    1018 TIL

---
+++ algorithm study/1018_baekjoon_5582.py
@@ -0,0 +1,34 @@
+'''
+PORPOISE
+PROPORTION
+'''
+
+# 공통 부분 문자열
+chars1 = input()
+chars2 = input()
+l1 = len(chars1)
+l2 = len(chars2)
+
+ismax = 0
+for i in range(l1):
+    cnt = 0
+    for j in range(l2-ismax):
+        compare1 = chars1[i:i+ismax]
+        compare2 = chars2[j:j+ismax]
+        if len(compare2) != len(compare1):
+            break
+        if compare1 == compare2:
+            di, dj = i+ismax, j+ismax
+            cnt += ismax
+            try:
+               while chars2[dj] == chars1[di]:
+                   cnt += 1
+                   dj += 1
+                   di += 1
+            except IndexError:
+               pass
+            if ismax < cnt:
+               ismax = cnt
+            cnt = 0
+
+print(ismax)
