e7e4fd9488972301805e084a042d2f04658802cc
dongchan <windcj1@naver.com>
2022-12-04 15:06:22 +0900

    1204

---
+++ algorithm study/prac.py
@@ -0,0 +1,11 @@
+# 월드컵
+
+score_board = [list(map(int, input().split())) for _ in range(4)]
+print(score_board)
+
+'''
+5 0 0 3 0 2 2 0 3 0 0 5 4 0 1 1 0 4
+4 1 0 3 0 2 4 1 0 1 1 3 0 0 5 1 1 3
+5 0 0 4 0 1 2 2 1 2 0 3 1 0 4 0 0 5
+5 0 0 3 1 1 2 1 2 2 0 3 0 0 5 1 0 4
+'''
