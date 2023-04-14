d9290c6d4c0ba6ed0587373c300ef11af161a5bb
chanshin0 <windcj1@naver.com>
2023-04-14 23:50:30 +0900

    0414 TIL

--- algorithm study/2023/prac.py
+++ algorithm study/2023/prac.py
@@ -1,21 +1,16 @@
-# 하늘에서 별똥별이 빗발친다
-N,M,L,K = map(int, input().split())
-stars = [list(map(int, input().split())) for _ in range(K)]
-blocked_star = 0
+n = int(input())
+arr = list(map(int, input().split()))
 
-def tram(x,y):
-    global blocked_star
-    cnt = 0
-    for i in range(x,x+L+1):
-        for j in range(y,y+L+1):
-            if [i,j] in stars:
-                cnt += 1
-            maximum = (x+L-i)*(y+L-j)
-            if cnt+maximum < blocked_star:
-                return
-    blocked_star = max(blocked_star, cnt)
-    return
+arr.sort()
 
-for x, y in stars:
-    tram(x,y)
-print(blocked_star)
+cnt = 0
+for i in range(2, n):
+    if arr[i] < 2:
+        continue
+
+    for j in range(i):
+        if arr[i] - arr[j] in arr[:i]:
+            cnt += 1
+            break
+
+print(cnt)
